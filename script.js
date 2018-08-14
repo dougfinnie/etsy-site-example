933ee9ae68431a8d48f462fe73c3a038fa002b72
Glitch (readonly-ravelry-api-example) <none>
2018-08-14 00:52:13 +0000

    🎡👵 Checkpoint ./script.js:312169/3233

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
 /* globals RavelryApi */
 
-window.ravelryApiClient = null;
-
 RavelryApi = function(base, authUsername, authPassword) {
   this.base = base;
   this.authUsername = authUsername;
   this.authPassword = authPassword;
-  
-  this.onErrorCallback = null;
 };
 
-RavelryApi.prototype.onError = function(callback) {
-  this.onErrorCallback = callback;
-};
 
 RavelryApi.prototype.get = function(url, callback) {
   const headers = new Headers();
   headers.append('Authorization', 'Basic ' + btoa(this.authUsername + ":" + this.authPassword));
   
-  const me = this;
-  fetch(url, { method: 'GET', headers: headers }).then(response => {
-    return response
-  }).catch(error => {
-  return Promise.reject(Error(error.message))
-})
+  return fetch(url, { method: 'GET', headers: headers });
 };
 
 RavelryApi.prototype.projectsList = function(username) {
@@ -35,21 +23,30 @@ RavelryApi.prototype.projectsList = function(username) {
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
+  let ravelryApiClient = null;
+
+  function renderProjects(json) {
+    ravelryApiClient.projectsList('frecklegirl').then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      var rootElement = document.getElementById('projects-list-results');
+      rootElement.innerHTML = '';
+      
+      json.projects.forEach(function(project) {
+        var child = document.createElement('DIV');
+        child.innerText = project.name;
+        rootElement.appendChild(child)
+      });
+    });  
+  };
+  
   const credentialsForm = document.getElementById('api-credentials-form');
   credentialsForm.onsubmit = function() {
     const username = credentialsForm.querySelector("input[name='username_key']").value;
     const password = credentialsForm.querySelector("input[name='password_key']").value;
     
-    window.ravelryApiClient = new RavelryApi('https://api.ravelry.com', username, password);
-
-    window.ravelryApiClient.onError(function(response) {
-      alert('An error occurred, check the console');
-      console.log(response);
-    });
-    
-    
-    console.log('a');
-    console.log(ravelryApiClient.projectsList('frecklegirl'));
+    ravelryApiClient = new RavelryApi('https://api.ravelry.com', username, password);
+    renderProjects();
     
     return false;
   };
