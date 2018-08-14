5b69470ca81613183517b99d7b28a75cabcdcab6
Glitch (readonly-ravelry-api-example) <none>
2018-08-14 00:31:36 +0000

    💰🍱 Checkpoint ./index.html:312169/158 ./script.js:312169/388

--- script.js
+++ script.js
@@ -18,7 +18,16 @@ RavelryApi.prototype.get = function(url, callback) {
   const headers = new Headers();
   headers.append('Authorization', 'Basic ' + btoa(this.authUsername + ":" + this.authPassword));
   
-  fetch(url, { method: 'GET', headers: headers }).then(callback);
+  const me = this;
+  fetch(url, { method: 'GET', headers: headers }).then(function(response) {
+    console.log(response);
+    if (response.status == 200) {
+      callback(response);
+    } else {
+      console.log("error");
+      me.onErrorCallback(response);
+    }
+  });
 };
 
 RavelryApi.prototype.projectsList = function(username) {
@@ -39,11 +48,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const password = credentialsForm.querySelector("input[name='password_key']").value;
     
     window.ravelryApiClient = new RavelryApi('https://api.ravelry.com', username, password);
-    window.ravelryApiClient.onError(function(errorMessage) {
+    window.ravelryApiClient.onError(function(response) {
       document.getElementById('api-success').style.display = 'none';
       const errorElement = document.getElementById('api-error');
       errorElement.style.display = 'block';
-      errorElement.innerHTML = errorMessage;
+      errorElement.innerHTML = response;
+      console.log(response);
     });
     
     console.log(ravelryApiClient.projectsList('frecklegirl'));
