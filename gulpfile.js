4c9f78247afc5b0cee6005fde108a289e26cc836
Glitch (readonly-ravelry-api-example) <none>
2020-04-21 22:40:40 +0000

    🎾🎒 Checkpoint ./package.json:987258/1663 ./gulpfile.js:987258/314

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
 const gulp = require('gulp');
+const del = require('del');
 
 gulp.task('clean', () => {
-    return del('public/style.css');
+     return del([
+       'public/fonts/**', '!public/fonts', 
+       'public/css/**', '!public/css', 
+       'public/js/**', '!public/js'
+     ]);
 });
 gulp.task('fonts', function() {
   return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/**.*')
       .pipe(gulp.dest('public/fonts'));
 });
 gulp.task('styles', function() {
-  return gulp.src('node_modules/@fortawesome/fontawesome-free/css/**.*')
+  return gulp.src('node_modules/@fortawesome/fontawesome-free/css/*.min.*')
       .pipe(gulp.dest('public/css'));
 });
 gulp.task('bootstrapcss', function() {
@@ -17,6 +22,6 @@ gulp.task('bootstrapcss', function() {
 });
 gulp.task('bootstrapjs', function() {
   return gulp.src('node_modules/bootstrap/dist/js/bootstrap.min.js')
-      .pipe(gulp.dest('public/css'));
+      .pipe(gulp.dest('public/js'));
 });
-gulp.task('default', gulp.series(['fonts', 'styles', 'bootstrapcss', 'bootstrapjs']));
+gulp.task('default', gulp.series(['clean', 'fonts', 'styles', 'bootstrapcss', 'bootstrapjs']));
